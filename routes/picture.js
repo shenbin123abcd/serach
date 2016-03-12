914e0380c57ec258f8eda1d48a6a2270a15180e4
野猪yz <1746534101@qq.com>
2016-03-12 18:33:31 +0800

    图片

--- routes/picture.js
+++ routes/picture.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 
 // 列表
-router.get('/', function(req, res, next){
-  res.render('picture_index', {title: '图片列表'});
+router.get(['/','/search'], function(req, res, next){
+    data={};
+    data.urlObj=url.parse(req.originalUrl);
+    data.route=data.urlObj.pathname.replace(req.baseUrl,'');
+    data.pageTitle='搜索引擎_幻熊科技 - 更智慧的婚礼业';
+    if(data.route=='/search'){
+
+    }
+
+
+    res.render('picture_index', {data: data});
 });
 
 // 详情
@@ -11,8 +21,10 @@ router.get('/detail/:id', function(req, res, next){
   res.render('picture_detail', {title: '图片详情'});
 });
 
+
 //结果
-router.get('/result', function(req, res, next){
+router.get('/se', function(req, res, next){
+
     res.render('picture_result', {title: '图片搜索结果'});
 });
 
