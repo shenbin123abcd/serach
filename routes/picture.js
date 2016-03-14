a3acf485eef36882ca15c3ba2291db6d2dbb880f
野猪yz <1746534101@qq.com>
2016-03-14 16:07:33 +0800

    图片首页

--- routes/picture.js
+++ routes/picture.js
@@ -3,17 +3,22 @@ var url = require('url');
 var router = express.Router();
 
 
-// 列表
+// 首页，列表，搜索
 router.get(['/','/search'], function(req, res, next){
-    data=data||{};
+    var data={};
 
-    data.urlObj=url.parse(req.originalUrl);
+    var urlObj=url.parse(req.originalUrl);
     data.baseUrl=req.baseUrl;
-    data.route=data.urlObj.pathname.replace(req.baseUrl,'');
+    data.route=urlObj.pathname.replace(req.baseUrl,'');
+    data.keyword=req.query.keyword;
 
-
-    data.pageTitle='搜索引擎_幻熊科技 - 更智慧的婚礼业';
-
+    if(data.route=='/search'){
+        data.pageTitle=`
+        '${req.query.keyword}' 的图片搜索结果-图片搜索
+        `;
+    }else{
+        data.pageTitle='图片首页';
+    }
 
     data.element=['全部','剪影','蝴蝶','春天','城堡','白云','星星','菱形','三角','雪花','樱花','卡通','纱幔','泡雕','纸花','蜡烛','蜡烛','明场',
         '灯珠','渐变','撞色','爱心','气球','飞屋','棉花','蕾丝','花墙','龙柳','拱门','树木','镜面','木桩','垂挂','架构','水晶','喷水池','满天星','向日葵',
@@ -29,14 +34,16 @@ router.get(['/','/search'], function(req, res, next){
 
 // 详情
 router.get('/detail/:id', function(req, res, next){
-  res.render('picture_detail', {title: '图片详情'});
+    var data={};
+
+    var urlObj=url.parse(req.originalUrl);
+    data.baseUrl=req.baseUrl;
+    data.route=urlObj.pathname.replace(req.baseUrl,'');
+
+    data.pageTitle='图片详情';
+    res.render('picture_detail', {data: data});
 });
 
 
-//结果
-router.get('/se', function(req, res, next){
-
-    res.render('picture_result', {title: '图片搜索结果'});
-});
 
 module.exports = router;
