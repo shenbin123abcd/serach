c585964d637ea57ced44c6535ab778b2f72a80f2
wtwei <wutwei@qq.com>
2016-03-15 18:52:03 +0800

    增加评论和收藏

---
+++ module/token.js
@@ -0,0 +1,65 @@
+/**
+ * Created by wtwei on 2015/10/30.
+ */
+//var redisClient = require('./redis_database').redisClient;
+var jwt = require("jsonwebtoken");
+var config = require('../config');
+var TOKEN_EXPIRATION = 60;
+var TOKEN_EXPIRATION_SEC = TOKEN_EXPIRATION * 60;
+
+var getToken = function(headers) {
+    if (headers && headers.authorization) {
+        var authorization = headers.authorization;
+        var part = authorization.split(' ');
+
+        if (part.length == 2) {
+            return part[1];
+        }
+        else {
+            return null;
+        }
+    }
+    else {
+        return null;
+    }
+};
+
+// 验证token
+exports.verifyToken = function (req, res, next) {
+    var path = req.url.split('/')[1];
+    if(config.NOT_CHECK_AUTH.indexOf(path) != -1){
+        next();
+    }else{
+        var token = getToken(req.headers);
+
+        jwt.verify(token,config.JWT_SECRET, function(err, decoded){
+            if(err){
+                res.status(401).json({iRet: -1, info: 'Access Denied'});
+            }else{
+                req.user = decoded;
+                next();
+            }
+        });
+    }
+};
+
+// 设置token过期
+exports.expireToken = function(headers) {
+    var token = getToken(headers);
+
+    if (token != null) {
+        redisClient.set(token, { is_expired: true });
+        redisClient.expire(token, TOKEN_EXPIRATION_SEC);
+    }
+};
+
+// 设置token缓存
+exports.setCache = function(data){
+    redisClient.hmset('TOKEN:' + data.token, data);
+};
+
+// 获取token缓存
+exports.getCache = function(){
+    return redisClient.hgetall('TOKEN:' + getToken());
+};
+
