79044e313bab27e202ce158c10b7ca9a3790ad7a
野猪yz <1746534101@qq.com>
2016-03-23 16:04:22 +0800

    company logo 4:3

--- app/public/js/app.companyDetail.js
+++ app/public/js/app.companyDetail.js
@@ -3,16 +3,25 @@ app.companyDetail=(function(){
 
     function toggleCases(){
 
+        $('#rest-cases-toggle__bt').on('click', function () {
+            $("#rest-cases-toggle__bt").remove();
+            $("#expand-more").show(300);
+
+        });
+
+
         $('#expand-more').on('shown.bs.collapse', function () {
-            $("#rest-cases-toggle__bt").html(`
-            <span class="text">收起</span> <i class="glyphicon glyphicon-menu-up"></i>
-            `);
+            //$("#rest-cases-toggle__bt").remove();
+
+            //$("#rest-cases-toggle__bt").html(`
+            //<span class="text">收起</span> <i class="glyphicon glyphicon-menu-up"></i>
+            //`);
         });
 
         $('#expand-more').on('hidden.bs.collapse', function () {
-            $("#rest-cases-toggle__bt").html(`
-            <span class="text">展开</span> <i class="glyphicon glyphicon-menu-down"></i>
-            `);
+            //$("#rest-cases-toggle__bt").html(`
+            //<span class="text">展开</span> <i class="glyphicon glyphicon-menu-down"></i>
+            //`);
         });
     }
 
