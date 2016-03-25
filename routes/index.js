48021e168cc98e09cec0c8d4d4b23e1cca31150a
wtwei <wutwei@qq.com>
2016-03-25 18:44:59 +0800

    删除注释代码

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var helper = require('../helper');
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-    req.redis.clear('search_index_data');
+    // req.redis.clear('search_index_data');
     req.redis.get('search_index_data').then(function(data){
         if (!data) {
             getData(req, res);
@@ -21,17 +21,6 @@ router.get('/', function(req, res, next){
 function getData(req, res){
     var params = {per_page: 100, group: 'company_id'}, data = {};
     // 最新案例
-    /*obj.getList('cases', req, params).then(function(body){
-        if (body.iRet === 1) {
-            data.case_new = helper.arrayShuffle(body.data.data, 13);
-            return data;
-        } else {
-            res.sendStatus(500);
-        }
-    }, function(error){
-        res.sendStatus(500);
-    })*/
-
     // 精选案例
     // params.per_page = 24;
     params['filter[recommended]'] = 1;
@@ -84,6 +73,38 @@ function getData(req, res){
             }
         })
     }).then(function(data){
+        data.case_new.forEach(function(n, i){
+            if(n.color==""){
+                n.color="#2797ff";
+            }else if(n.color=="#ffffff"){
+                n.fontColor="#333";
+            }else{
+                n.fontColor="#ffffff";
+            }
+            if(i < 2){
+                n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/420/h/300";
+            }else if(i == 2){
+                n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/200/h/300";
+            }else{
+                n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/200/h/150";
+            }
+        });
+
+        data.case_recommend.forEach(function(n, i){
+            n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/200/h/150"
+        });
+        data.zhuanti.forEach(function(n, i){
+            n.default_image = req.config.url.case + '/' + n.default_image + "?imageView2/1/w/420/h/226"
+        });
+        data.image.forEach(function(n, i){
+            n.path = req.config.url.case + '/' + n.path + "?imageView2/1/w/200/h/150"
+        });
+        data.company.forEach(function(n, i){
+            n.logo = req.config.url.company + '/' + n.logo + "?imageView2/1/w/200/h/150"
+        });
+        data.baseUrl = req.baseUrl;
+        data.pageTitle = '搜索引擎-首页';
+
         req.redis.set('search_index_data', JSON.stringify(data), 86400);
 
         render(data, req, res);
@@ -91,43 +112,7 @@ function getData(req, res){
 }
 
 function render(data, req, res){
-    data.case_new.forEach(function(n, i){
-        if(n.color==""){
-            n.color="#2797ff";
-        }else if(n.color=="#ffffff"){
-            n.fontColor="#333";
-        }else{
-            n.fontColor="#ffffff";
-        }
-        if(i < 2){
-            n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/420/h/300";
-        }else if(i == 2){
-            n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/200/h/300";
-        }else{
-            n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/200/h/150";
-        }
-    });
-    /*data.case_new[0].cover = data.case_new[0].cover + "?imageView2/1/w/420/h/300";
-    data.case_new[1].cover = data.case_new[1].cover + "?imageView2/1/w/420/h/300";
-    data.case_new[2].cover = data.case_new[2].cover + "?imageView2/1/w/200/h/300";
-    for (var i = 3; i < 13; i++) {
-        data.case_new[i].cover = data.case_new[i].cover + "?imageView2/1/w/200/h/150";
-    }*/
-    data.case_recommend.forEach(function(n, i){
-        n.cover = req.config.url.case + '/' + n.cover + "?imageView2/1/w/200/h/150"
-    });
-    data.zhuanti.forEach(function(n, i){
-        n.default_image = req.config.url.case + '/' + n.default_image + "?imageView2/1/w/420/h/226"
-    });
-    data.image.forEach(function(n, i){
-        n.path = req.config.url.case + '/' + n.path + "?imageView2/1/w/200/h/150"
-    });
-    data.company.forEach(function(n, i){
-        n.logo = req.config.url.company + '/' + n.logo + "?imageView2/1/w/200/h/150"
-    });
-    data.baseUrl = req.baseUrl;
-    data.pageTitle = '搜索引擎-首页';
-    //res.json(data);
+
     res.render('index', {data: data});
 }
 
