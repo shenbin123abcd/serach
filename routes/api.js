22a3a5ed1a07bf611e38df2fe353378ea3959c92
野猪yz <1746534101@qq.com>
2016-04-08 12:18:27 +0800

    hotel_index

--- routes/api.js
+++ routes/api.js
@@ -58,5 +58,21 @@ router.get('/region/parent', function(req, res){
         res.sendStatus(500);
     });
 });
+// 获取地区同级信息
+router.get('/region/siblings', function(req, res){
+    var id = req.query.id || 0;
+    obj.get('region/siblings/' + id,req).then(function(body){
+        if (body.iRet === 1) {
+            res.json({iRet: 1, info: 'success', data: body.data});
+        } else if (body.iRet === 0) {
+            res.json({iRet: 0, info: 'failed', error: body.info});
+        } else {
+            res.sendStatus(500);
+        }
+    }, function(error){
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
