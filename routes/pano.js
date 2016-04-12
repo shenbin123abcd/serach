a09450edff4b2e5cf3306f33d2f9cf1d15346adb
edward <edward_wenj@163.com>
2016-04-12 11:16:40 +0800

    酒店全景 - krpano 1.16 更新 1.19 pre3

--- routes/pano.js
+++ routes/pano.js
@@ -7,29 +7,62 @@ var config = require('../config');
 var obj = require('../module/module');
 
 
-var xml_pre1 = '<krpano version="1.16" title="{title}" onstart="startup();">';
+var xml_pre1 = '<krpano version="1.19" title="{title}">';
 var xml_pre2 = '<include url="skin/vtourskin.xml" />\
-    <skin_settings bingmaps="false"\
-        bingmaps_key=""\
-        bingmaps_zoombuttons="false"\
+    <skin_settings maps="false"\
+        maps_type="bing"\
+        maps_bing_api_key=""\
+        maps_zoombuttons="false"\
         gyro="true"\
-        thumbs_width="120" thumbs_height="80" thumbs_padding="10" thumbs_crop="0|40|240|160"\
+        webvr="true"\
+        littleplanetintro="false"\
+        title="true"\
+        thumbs="true"\
+        thumbs_width="120" thumbs_height="80" thumbs_padding="10"\
+        thumbs_crop="0|40|240|160"\
         thumbs_opened="false"\
         thumbs_text="false"\
         thumbs_dragging="true"\
         thumbs_onhoverscrolling="false"\
         thumbs_scrollbuttons="false"\
         thumbs_scrollindicator="false"\
+        thumbs_loop="false"\
+        tooltips_buttons="false"\
         tooltips_thumbs="false"\
         tooltips_hotspots="false"\
         tooltips_mapspots="false"\
+        deeplinking="false"\
+        loadscene_flags="MERGE"\
+        loadscene_blend="OPENBLEND(0.5, 0.0, 0.75, 0.05, linear)"\
+        loadscene_blend_prev="SLIDEBLEND(0.5, 180, 0.75, linear)"\
+        loadscene_blend_next="SLIDEBLEND(0.5,   0, 0.75, linear)"\
+        loadingtext="loading..."\
+        layout_width="100%"\
+        layout_maxwidth="814"\
+        controlbar_width="-24"\
+        controlbar_height="40"\
         controlbar_offset="20"\
+        controlbar_offset_closed="-40"\
+        controlbar_overlap.no-fractionalscaling="10"\
+        controlbar_overlap.fractionalscaling="0"\
+        design_skin_images="vtourskin.png"\
+        design_bgcolor="0x2D3E50"\
+        design_bgalpha="0.8"\
+        design_bgborder="0"\
+        design_bgroundedge="1"\
+        design_bgshadow="0 4 10 0x000000 0.3"\
+        design_thumbborder_bgborder="3 0xFFFFFF 1.0"\
+        design_thumbborder_padding="2"\
+        design_thumbborder_bgroundedge="0"\
+        design_text_css="color:#FFFFFF; font-family:Arial;"\
+        design_text_shadow="0"\
     />\
-    <layer name="skin_logo" url="" scale="0.25" opened_onclick="openurl(\'...\',_blank);" />\
-    <action name="startup">\
-        if(startscene === null, copy(startscene,scene[0].name));\
+    <action name="startup" autorun="onstart">\
+        if(startscene === null OR !scene[get(startscene)], copy(startscene,scene[0].name); );\
         loadscene(get(startscene), null, MERGE);\
+        if(startactions !== null, startactions() );\
     </action>';
+
 var xml_scene1 = '<scene name="h{id}" title="{title}" onstart="" thumburl="{root_url}/thumb.jpg" lat="" lng="" heading="">';
 var xml_scene2 = '<view hlookat="{hlookat}" vlookat="{vlookat}" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto" />';
 var xml_scene3 = '<preview url="{root_url}/preview.jpg" />';
